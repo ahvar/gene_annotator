276fc81fc65e061359a79ba55458902197fc9729
Arthur Vargas <arthurvargas@Arthurs-MacBook-Air.local>
2026-01-20 13:06:23 -0600

    update email handler for sending emails about background process completion

--- src/app/email_service.py
+++ src/app/email_service.py
@@ -9,10 +9,24 @@ def send_async_email(app, msg):
         mail.send(msg)
 
 
-def send_email(subject, sender, recipients, text_body, html_body):
+def send_email(
+    subject, sender, recipients, text_body, html_body, attachments=None, sync=False
+):
+    """
+    When sending an email from a background task, like an export service, which is already
+    asynchronous, we'll send them in the foreground. The attach method of Message accepts
+    three arguments that define an attachment: filename, media type, actual file data. The
+    filename is just the name the recipient will see, it does not need to be a real file.
+    """
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
-    Thread(
-        target=send_async_email, args=(current_app._get_current_object(), msg)
-    ).start()
+    if attachments:
+        for attachment in attachments:
+            msg.attach(*attachment)
+    if sync:
+        mail.send(msg)
+    else:
+        Thread(
+            target=send_async_email, args=(current_app._get_current_object(), msg)
+        ).start()
